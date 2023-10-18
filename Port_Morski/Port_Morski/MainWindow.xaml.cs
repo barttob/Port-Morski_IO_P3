812e2dd517c2d9f6f379fb913a9efd97e1d34a53
KarolR2001 <karolrembiasz@o2.pl>
2023-10-18 17:23:56 +0200

    Inicjacja wyglądu zakładki wraz z zakładkami.

--- Port_Morski/Port_Morski/MainWindow.xaml.cs
+++ Port_Morski/Port_Morski/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Port_Morski.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Port_Morski
 {
     /// <summary>
@@ -23,6 +25,53 @@ namespace Port_Morski
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+        }
+
+        
+
+        private void StatystykiButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            Statystyki statystyki = new Statystyki();
+            MainGrid.Children.Add(statystyki);
+        }
+
+        private void MonitorowanieStatkow_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            MonitorowanieStatkow monitorowanie = new MonitorowanieStatkow();
+            MainGrid.Children.Add(monitorowanie);
+        }
+
+        private void ZarzadzanieLadunkami_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            ZarzadzanieLadunkami zarzadzanieLadunkami = new ZarzadzanieLadunkami();
+            MainGrid.Children.Add(zarzadzanieLadunkami);
+        }
+
+        private void PlanowanieOperacjiPortowych_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            PlanowanieOperacjiPortowych planowanie = new PlanowanieOperacjiPortowych();
+            MainGrid.Children.Add(planowanie);
+        }
+
+
+        private void GenerowanieRaportow_Click(object sender, RoutedEventArgs e)
+        {
+            MainGrid.Children.Clear();
+            GenerowanieRaportow generowanieRaportow = new GenerowanieRaportow();
+            MainGrid.Children.Add(generowanieRaportow);
+        }
+
+        private void Administracja_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MainGrid.Children.Clear();
+            Administracja administracja = new Administracja();
+            MainGrid.Children.Add(administracja);
         }
     }
 }
