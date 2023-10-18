46549fd11414ee8541f81ddf9cded465de67f05c
”Skraba_Paweł” <pawel2001@vp.pl>
2023-10-18 18:08:04 +0200

    Add DB models

---
+++ Port_Morski/Port_Morski/Models/EmpSchedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Port_Morski.Models;
+
+public partial class EmpSchedule
+{
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public string? ShiftType { get; set; }
+
+    public virtual User? User { get; set; }
+}
---
+++ Port_Morski/Port_Morski/Models/Ship.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Port_Morski.Models;
+
+public partial class Ship
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Type { get; set; }
+
+    public int? Capacity { get; set; }
+
+    public virtual ICollection<Cargo> Cargos { get; set; } = new List<Cargo>();
+
+    public virtual ICollection<ShipSchedule> ShipSchedules { get; set; } = new List<ShipSchedule>();
+}
